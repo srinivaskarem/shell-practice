a556aba9751d7034cb9c100ea6641ad982ddbc36
srinivaskarem <srinu007vas@gmail.com>
2025-05-21 06:22:58 +0530

    hello world started

---
+++ 02-conversation.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Trump: Hey musk, How are you"
+echo "musk: Hellow Trump, I am fine. how are you doing"
+echo "Trump: fine, whatsapp"
+echo "musk: going to march will come?"
+echo "Trump:no once you come back i will join"
+
